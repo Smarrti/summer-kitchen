9cb03c21480c2018391143c6c4a9f03cecf8ca8e
Sergey Bashkin <bashkin2013@mail.ru>
2020-08-26 22:48:22 +0400

    Добавлена функциональность к кнопкам

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 import './swiper.min';
-// import Swiper from 'swiper';
 import '../css/style.scss';
 import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
 
+const body = document.querySelector('body');
 const swiper = new Swiper('.team-content__slider', {
   pagination: {
     el: '.swiper-pagination',
@@ -12,4 +12,20 @@ const swiper = new Swiper('.team-content__slider', {
     nextEl: '.slide__arrows_forward',
     prevEl: '.slide__arrows_back',
   },
+  loop: true,
 });
+
+body.addEventListener('click', (e) => {
+  e.preventDefault();
+  const { target } = e;
+  switch (true) {
+    case target.classList.contains('slide__arrows_forward'):
+      swiper.slideNext();
+      break;
+    case target.classList.contains('slide__arrows_back'):
+      swiper.slidePrev();
+      break;
+    default:
+      break;
+  }
+})
